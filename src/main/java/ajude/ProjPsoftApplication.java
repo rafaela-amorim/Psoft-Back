b65f3131b9a4b4c3430eb818fe5bca090321fb45
Gabryelle Soares <gabryelle.almeida@ccc.ufcg.edu.br>
2019-10-28 14:22:18 -0300

    projeto gerado com Web, JPA e H2

---
+++ src/main/java/ajude/ProjPsoftApplication.java
@@ -0,0 +1,13 @@
+package ajude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjPsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjPsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajude/ProjPsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ajude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjPsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
