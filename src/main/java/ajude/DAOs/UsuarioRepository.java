27a142133791d3da36461d38bf101687c3e1171a
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2019-10-28 15:06:51 -0300

    UsuarioRepository adicionado

---
+++ src/main/java/ajude/DAOs/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package ajude.DAOs;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ajude.entities.Usuario;
+
+
+public interface UsuarioRepository<T, ID extends Serializable> extends JpaRepository<Usuario, String>  {
+
+}
