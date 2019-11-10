12ab07c1daa9c62804077bba721af0cb21174608
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2019-11-10 19:26:02 -0300

    adição do cors para permitir comunicação cross origin

--- src/main/java/ajude/ProjPsoftApplication.java
+++ src/main/java/ajude/ProjPsoftApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import ajude.filter.Filter;
 
 @SpringBootApplication
 public class ProjPsoftApplication {
+	
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 	
 	@Bean
 	public FilterRegistrationBean<Filter> filter() {
